510263fc33fc2147e2c5bfae6d93f8fc30903e19
pebra <self@pebra.net>
2015-04-20 14:40:52 +0200

    Remove hint for Bank Transfer on Checkout. Add German (incomplete) Translations

--- app/overrides/add_bank_details_form_to_order_show.rb
+++ app/overrides/add_bank_details_form_to_order_show.rb
@@ -1,12 +1,12 @@
-Deface::Override.new(
-  :virtual_path => 'spree/shared/_order_details',
-  :name => 'add_bank_details_form_to_order_show',
-  :insert_bottom => ".payment-info",
-  :text => %q{
-    <% if bank_transfer_payment = @order.payments.from_bank_transfer.first %>
-        <br>
-        <%= link_to Spree.t(:add_details), "https://docs.google.com/viewerng/viewer?url=https://s3-eu-west-1.amazonaws.com/spreeimagesproduits/RIBGUIXMODEL.pdf", :class => 'button add_bt_details', target: "_blank" %>
-        <br>
-    <% end %>
-  }
-)
+#Deface::Override.new(
+#  :virtual_path => 'spree/shared/_order_details',
+#  :name => 'add_bank_details_form_to_order_show',
+#  :insert_bottom => ".payment-info",
+#  :text => %q{
+#    <% if bank_transfer_payment = @order.payments.from_bank_transfer.first %>
+#        <br>
+#        <%= link_to Spree.t(:add_details), "https://docs.google.com/viewerng/viewer?url=https://s3-eu-west-1.amazonaws.com/spreeimagesproduits/RIBGUIXMODEL.pdf", :class => 'button add_bt_details', target: "_blank" %>
+#        <br>
+#    <% end %>
+#  }
+#)
